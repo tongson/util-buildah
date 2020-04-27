52870fa866ccd63b08ca50ad1dcc26f3359755d2
Eduardo Tongson <propolice@gmail.com>
2020-04-27 20:45:38 +0800

    New executables for wiping empty directories, docs, Debian dpkg/apt and userland(coreutils etc.)

---
+++ cmd/wipe_docs/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strings"
+)
+
+const files string = `/usr/share/doc
+/usr/share/man                                                        
+/usr/share/menu
+/usr/share/groff
+/usr/share/info
+/usr/share/lintian
+/usr/share/linda
+/usr/share/bug
+/usr/share/locale
+/usr/share/bash-completion
+/var/cache/man`
+
+func main() {
+	s := bufio.NewScanner(strings.NewReader(files))
+	for s.Scan() {
+		os.RemoveAll(s.Text())
+	}
+}
