f91d3d1acc6a0c3a8e252d7ca4e9fb080cd6873d
Eduardo Tongson <propolice@gmail.com>
2020-04-29 12:27:05 +0000

    For removing perl.

---
+++ cmd/wipe_perl/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"strings"
+	"bufio"
+	"os"
+	"path/filepath"
+)
+
+const wildcard string = `/usr/bin/perl*`
+const files string = `/usr/lib/x86_64-linux-gnu/perl-base`
+
+func main() {
+	ws := bufio.NewScanner(strings.NewReader(wildcard))
+	for ws.Scan() {
+		glob, _ := filepath.Glob(ws.Text())
+		for _, f := range glob {
+			os.Remove(f)
+		}
+	}
+	s := bufio.NewScanner(strings.NewReader(files))
+	for s.Scan() {
+		os.RemoveAll(s.Text())
+	}
+}
